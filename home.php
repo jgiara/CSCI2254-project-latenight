43cf645537f6591d648e8a361ee5a8c9f499eadb
maxwell-andrew <andrewmr@bc.edu>
2016-04-05 15:57:56 -0400

    Home Page
    First commit

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Late Night</title>
+		<link type="text/css" rel="stylesheet" href="css/home.css">
+		<link href='https://fonts.googleapis.com/css?family=Ubuntu:400,300' rel='stylesheet' type='text/css'>
+	</head>
+
+	<body>
+		<div>
+			<h1>Late Night Delivery Service</h1>
+			<h2>Coming to a dining hall near you...</h2>
+		</div>
+	</body>
+</html>
