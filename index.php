400c94ca300a3470f427934e343134d8f8838174
csendler <christian.sendler@gmail.com>
2016-04-07 15:49:02 -0400

    todo

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Late Night</title>
+		<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
 		<link type="text/css" rel="stylesheet" href="css/home.css">
 		<link href='https://fonts.googleapis.com/css?family=Ubuntu:400,300' rel='stylesheet' type='text/css'>
 	</head>
@@ -12,5 +13,8 @@
 			<h1>Late Night Delivery Service</h1>
 			<h2>Coming to a dining hall near you...</h2>
 		</div>
+
+
+	<script type="text/javascript"></script>
 	</body>
 </html>
