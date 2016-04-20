93d1b5b65c54ce8ed2210e4c337bf14df838f834
csendler <christian.sendler@gmail.com>
2016-04-20 11:42:16 -0400

    progress...

--- user/userHome.php
+++ user/userHome.php
@@ -5,14 +5,87 @@
 	<meta charset="utf-8">
   	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Home | Project Late Night</title>
+	<title>Select Role | Project Late Night</title>
 	<meta name="description" content="Boston College Late Night Delivery">
 
- 	<link href="css/bootstrap.min.css" rel="stylesheet">
- 	<link rel="stylesheet" href="css/home.css">
+ 	<link href="../css/bootstrap.min.css" rel="stylesheet">
+ 	<link rel="stylesheet" href="../css/home.css">
 
 </head>
-<body>
-	
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="./index.php">Late Night Delivery</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                  
+                    <li class="hidden">
+                        <a class="page-scroll" href="#page-top"></a>
+                    </li>
+                    
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <!-- Intro Section -->
+    <section id="select" class="intro-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-lg-offset-3">
+                    <h1>What would you like to do?</h1>
+                    <a type="button" class="btn btn-lg btn-success btn-block" href="./deliverer/deliveryHome.php">Place an Order</a>
+                    <a type="button" class="btn btn-lg btn-warning btn-block" href="./user/userHome.php">Review Orders</a>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-lg-6 col-lg-offset-3">
+                    <a type="button" class="btn btn-lg btn-success btn-block" href="./deliverer/deliveryHome.php">Track Order</a>
+                    <a type="button" class="btn btn-lg btn-warning btn-block" href="./user/userHome.php">Manage Settings</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<!-- scripts & BS/custom JS -->
+
+    <script src="../js/jquery.easing.min.js"></script>
+	<script src="../js/scripts.js"></script>
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  	<script src="../js/bootstrap.min.js"></script>
+  	<script type="text/javascript"> 
+		 $(window).scroll(function() {
+		    if ($(".navbar").offset().top > 50) {
+		        $(".navbar-fixed-top").addClass("top-nav-collapse");
+		    } else {
+		        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+		    }
+		});
+
+		//jq for page scroll, using hte easing lib
+		$(function() {
+		    $('a.page-scroll').bind('click', function(event) {
+		        var $anchor = $(this);
+		        $('html, body').stop().animate({
+		            scrollTop: $($anchor.attr('href')).offset().top
+		        }, 1500, 'easeInOutExpo');
+		        event.preventDefault();
+		    });
+		});
+  	</script>
 </body>
 </html>
