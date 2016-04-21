122fab1b564d3ddef7d6846f045de83bfbb857c9
Jon Giara <giara@bc.edu>
2016-04-20 23:39:26 -0400

    Insert Statements

---
+++ projectDocs/populateTables.sql
@@ -0,0 +1,120 @@
+#Populate Users Table
+
+
+#Populate Reviews Table
+
+
+#Populate Orders Table
+
+
+#Populate Items Table
+#Mac Items
+insert into Items (Name, Price, Availability)
+values
+(Chicken Tenders, 6.49, 'Mac');
+insert into Items (Name, Price, Availability)
+values
+(French Fries, 2.50, 'Mac');
+insert into Items (Name, Price, Availability)
+values
+(Mozzarella Sticks, 5.70, 'Mac');
+insert into Items (Name, Price, Availability)
+values
+(Pasta, 4.90, 'Mac');
+insert into Items (Name, Price, Availability)
+values
+(Pasta & Meatballs, 7.20, 'Mac');
+insert into Items (Name, Price, Availability)
+values
+(Onion Rings, 2.50, 'Mac');
+insert into Items (Name, Price, Availability)
+values
+(Grilled Chicken, 7.55, 'Mac');
+insert into Items (Name, Price, Availability)
+values
+(Burger, 8.00, 'Mac');
+insert into Items (Name, Price, Availability)
+values
+(Pizza, 5.39, 'Mac');
+
+#Stuart Items
+insert into Items (Name, Price, Availability)
+values
+(Chicken Tenders, 6.49, 'Stuart');
+insert into Items (Name, Price, Availability)
+values
+(French Fries, 2.50, 'Stuart');
+insert into Items (Name, Price, Availability)
+values
+(Mozzarella Sticks, 5.70, 'Stuart');
+insert into Items (Name, Price, Availability)
+values
+(Pasta, 4.90, 'Stuart');
+insert into Items (Name, Price, Availability)
+values
+(Pasta & Meatballs, 7.20, 'Stuart');
+insert into Items (Name, Price, Availability)
+values
+(Onion Rings, 2.50, 'Stuart');
+insert into Items (Name, Price, Availability)
+values
+(Grilled Chicken, 7.55, 'Stuart');
+insert into Items (Name, Price, Availability)
+values
+(Burger, 8.00, 'Stuart');
+insert into Items (Name, Price, Availability)
+values
+(Pizza, 5.39, 'Stuart');
+
+#Lower Items
+insert into Items (Name, Price, Availability)
+values
+(Chicken Tenders, 6.49, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(French Fries, 2.50, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Mozzarella Sticks, 5.70, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Pasta, 4.90, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Pasta & Meatballs, 7.20, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Onion Rings, 2.50, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Grilled Chicken, 7.55, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Burger, 8.00, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Pizza, 5.39, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Caesar Wrap, 6.50, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Honey Q Wrap, 6.50, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Chicken Parm Sub, 7.29, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Buffalo Chicken Sub, 5.39, 'Lower');
+insert into Items (Name, Price, Availability)
+values
+(Steak & Cheese, 7.69, 'Lower');
+
+
+
+
+
+
+
+
+#Populate Order_Items Table
