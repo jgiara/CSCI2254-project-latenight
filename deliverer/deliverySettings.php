4a66ee818741b7f4946ce7eac173331ef350a7b7
csendler <christian.sendler@gmail.com>
2016-05-04 21:14:02 -0400

    nav

--- deliverer/deliverySettings.php
+++ deliverer/deliverySettings.php
@@ -59,11 +59,11 @@ echo "<input type='hidden' id='userphone' value='$phn'/>";
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Navigation <span class="fa fa-angle-down"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="./userHome.php">Home</a></li>
+            <li><a href="./deliveryHome.php">Home</a></li>
             <li><a href="../typeSelect.php">Switch Account</a></li>
-            <li><a href="./userSettings.php">Settings</a></li>
+            <li><a href="./deliverySettings.php">Settings</a></li>
             <li role="separator" class="divider"></li>
-            <li><a href="../index.php">Sign Out</a></li>
+            <li><a href="../logout.php">Sign Out</a></li>
           </ul>
         </li>
       </ul>
